164df5b4fd3c7a354df59f98dd8ed18e18dadf6e
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-02-28 20:12:14 +0600

    Refactoring product controller

--- techno-space-back/src/main/kotlin/usrt/technospace/controllers/ProductController.kt
+++ techno-space-back/src/main/kotlin/usrt/technospace/controllers/ProductController.kt
@@ -1,6 +1,10 @@
 package usrt.technospace.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.*
 import usrt.technospace.models.product.Product
 import usrt.technospace.repository.ProductRepository
@@ -12,13 +16,37 @@ class ProductController {
     @Autowired
     private lateinit var productRepository: ProductRepository
 
-    @GetMapping("/products/{productId}")
-    fun getProduct(@PathVariable productId: Long): Product {
-        return productRepository.getOne(productId)
-    }
-
     @PostMapping("/products")
     fun addProduct(@Valid @RequestBody product: Product): Product {
         return productRepository.save(product)
+    }
+
+    @GetMapping("/products/{id}")
+    fun getProduct(@PathVariable id: Long): Product {
+        return productRepository.getOne(id)
+    }
+
+    @PutMapping("/products")
+    fun updateProduct(@Valid @RequestBody product: Product): Product {
+        return productRepository.save(product)
+    }
+
+    @GetMapping("/products")
+    fun getProductsByProductTypeId(
+            @RequestParam("productTypeId")
+            productTypeId: Long?,
+            @PageableDefault(sort = ["createdAt"], direction = Sort.Direction.DESC)
+            pageable: Pageable): Page<Product> {
+
+        if (productTypeId == null) {
+            return productRepository.findAll(pageable)
+        }
+
+        return productRepository.findAllByProductTypeId(productTypeId, pageable)
+    }
+
+    @GetMapping("/products/count")
+    fun getProductsCount(@RequestParam("productTypeId") productTypeId: Long): Long {
+        return productRepository.countProductsByProductTypeId(productTypeId)
     }
 }
