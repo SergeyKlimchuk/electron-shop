319546ad5b3a0f4edc4332307f5c64f1f63dbf25
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-03-03 16:08:24 +0600

    Remove useless imports

--- techno-space-back/src/main/kotlin/usrt/technospace/controllers/ProductInfoValueController.kt
+++ techno-space-back/src/main/kotlin/usrt/technospace/controllers/ProductInfoValueController.kt
@@ -1,10 +1,6 @@
 package usrt.technospace.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
-import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.*
 import usrt.technospace.models.product.ProductInfoValue
 import usrt.technospace.repository.ProductInfoValueRepository
