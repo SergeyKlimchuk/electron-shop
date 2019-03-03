0ea3616153b423c2bb0c7a9de6d702a4e6f0c703
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-03-03 20:54:16 +0600

    Add dictionary and dictionary values

---
+++ techno-space-front/src/models/products/dictionary-value.ts
@@ -0,0 +1,7 @@
+import { Dictionary } from './dictionary';
+
+export class DictionaryValue {
+  id: number;
+  name: string;
+  dictionary: Dictionary;
+}
---
+++ techno-space-front/src/models/products/dictionary.ts
@@ -0,0 +1,7 @@
+import { DictionaryValue } from './dictionary-value';
+
+export class Dictionary {
+  id: number;
+  name: string;
+  values: DictionaryValue[];
+}
