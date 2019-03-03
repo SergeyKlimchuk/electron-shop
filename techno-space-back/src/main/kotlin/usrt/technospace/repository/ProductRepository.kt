1a814128afba1a107b4a2ab27ff99a98b43f754a
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-03-03 15:45:09 +0600

    Add pageable find for products

--- techno-space-back/src/main/kotlin/usrt/technospace/repository/ProductRepository.kt
+++ techno-space-back/src/main/kotlin/usrt/technospace/repository/ProductRepository.kt
@@ -1,5 +1,7 @@
 package usrt.technospace.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import usrt.technospace.models.product.Product
@@ -8,7 +10,9 @@ import usrt.technospace.models.product.Product
 @Repository
 interface ProductRepository : JpaRepository<Product, Long> {
 
-    fun findByProductTypeId(id: Long): List<Product>
+    fun countProductsByProductTypeId(id: Long): Long
 
     fun findByNameContainingIgnoreCase(name: String): List<Product>
+
+    fun findAllByProductTypeId(productTypeId: Long, pageable: Pageable): Page<Product>
 }
