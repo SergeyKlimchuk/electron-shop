3234352ed6cafaff223d8b61bc3d7fcb48f04ca2
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-03-08 13:12:24 +0600

    Create product full info page

--- techno-space-front/src/app/pages/page-product/page-product.component.ts
+++ techno-space-front/src/app/pages/page-product/page-product.component.ts
@@ -1,4 +1,11 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
+import { Product } from 'src/models/products/product';
+import { ProductProperty } from 'src/models/products/product-property';
+import { ProductInfoValueService } from 'src/app/services/product-info-value/product-info-value.service';
+import { ProductInfoTitleService } from 'src/app/services/product-info-title/product-info-title.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-page-product',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageProductComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product();
+  displayedColumns = ['name', 'value'];
+  properties: ProductProperty[];
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService,
+              private productInfoValueService: ProductInfoValueService,
+              private productInfoTitleService: ProductInfoTitleService) { }
 
   ngOnInit() {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const productId = this.route.snapshot.paramMap.get('productId');
+    this.productService.getProduct(Number(productId)).subscribe(
+      (product) => {
+        this.product = product;
+        console.log(product);
+        this.applyproductProperties();
+      },
+      (error) => {
+        console.error(error);
+        //this.redirectToMain();
+      }
+    );
+  }
+
+  redirectToMain() {
+    this.router.navigate(['/main']);
+  }
+
+  async applyproductProperties() {
+    this.productService.getProductProperties(this.product.id).subscribe(
+      (properties) => {
+        console.log(properties);
+        this.properties = new Array<ProductProperty>();
+        this.properties = properties;
+      },
+      (error) => {
+        console.error('Не удалось получить свойства товара!', error);
+      }
+    );
   }
 
 }
