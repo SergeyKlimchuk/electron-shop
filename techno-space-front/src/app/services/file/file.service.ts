fa194171a11fc20176536e82db39a928e3293230
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-03-09 17:23:30 +0600

    Add service for work with files on front

---
+++ techno-space-front/src/app/services/file/file.service.ts
@@ -0,0 +1,17 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  constructor(private http: HttpClient) { }
+
+  uploadFile(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+    return this.http.post<any>('/api/files', formData);
+  }
+}
