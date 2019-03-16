d85838f1b1b05748bd04909a65da8e7a0cc5cddd
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-03-16 13:02:35 +0600

    Add edit product panel

---
+++ techno-space-front/src/app/pages/page-admin/subpages/product-subpage/edit-product/edit-product-dialog.ts
@@ -0,0 +1,136 @@
+import { DictionaryService } from 'src/app/services/dictionary/dictionary.service';
+import { Subject } from 'rxjs';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatTableDataSource, MatSnackBar } from '@angular/material';
+import { LoadableImageComponent } from 'src/app/core/loadable-image/loadable-image.component';
+import { ProductInfoTitleService } from 'src/app/services/product-info-title/product-info-title.service';
+import { ProductInfoValueService } from 'src/app/services/product-info-value/product-info-value.service';
+import { ProductService } from 'src/app/services/product/product.service';
+import { Product } from 'src/models/products/product';
+import { ProductInfoTitle } from 'src/models/products/product-info-title';
+
+import { ProductInfoValue } from './../../../../../../models/products/product-info-text';
+import { ProductType } from 'src/models/products/product-type';
+import { ProductTypeService } from 'src/app/services/product-type/product-type.service';
+
+@Component({
+  selector: 'app-edit-product',
+  templateUrl: './edit-product-dialog.html',
+  styleUrls: ['./edit-product-dialog.styl']
+})
+export class EditProductDialog implements OnInit {
+
+  @ViewChild(LoadableImageComponent)
+  loadableImage: LoadableImageComponent;
+
+  product = new Product();
+  productTypes$ = new Subject<ProductType[]>();
+
+  action: string;
+  propertiesDataSource = new MatTableDataSource<{title: ProductInfoTitle, value: ProductInfoValue}>();
+  displayedCollumns = ['title', 'value'];
+
+  image = {
+    // url: 'https://cdn-images-1.medium.com/max/800/1*pOvzhe0JaYBd6yLooq2fzA.png'
+    url: '/'
+  };
+
+  constructor(public dialogRef: MatDialogRef<EditProductDialog>,
+              @Inject(MAT_DIALOG_DATA) public productId: number,
+              private productService: ProductService,
+              private snack: MatSnackBar,
+              private dictionaryService: DictionaryService,
+              private productTypeService: ProductTypeService,
+              private productInfoTitleService: ProductInfoTitleService,
+              private productInfoValueService: ProductInfoValueService) {
+
+    if (productId == null) {
+      this.action = 'Создание товара';
+    } else {
+      this.action = 'Редактирование товара';
+      this.productService.getProduct(productId).subscribe(
+        product => {
+          this.product = product;
+          this.loadProperties();
+        },
+        error => {
+          alert('Ошибка');
+          console.error(error);
+        }
+      );
+    }
+
+    this.loadProductTypes();
+  }
+
+  loadProductTypes() {
+    this.productTypeService.getProductTypes(0, 30).subscribe(
+      productTypes => {
+        this.productTypes$.next(productTypes.content);
+        console.log('Загружены типы продуктов', productTypes);
+      },
+      error => {
+        alert('Ошибка');
+        console.error(error);
+      }
+    );
+  }
+
+  onSelectProductType(productType: ProductType) {
+    this.product.productType = productType;
+    console.log('Начата загрузка свойств', productType);
+    this.loadProperties();
+  }
+
+  async loadProperties() {
+    const productTypeId = this.product.productType.id;
+    const titles = await this.productInfoTitleService.getProductInfoTitles(productTypeId).toPromise();
+    if (this.productId == null) {
+      this.updateDataSource(titles, null);
+    } else {
+      const values = await this.productInfoValueService.getProductInfoValues(this.productId).toPromise();
+      this.updateDataSource(titles, values);
+    }
+  }
+
+  updateDataSource(titles: ProductInfoTitle[], values: ProductInfoValue[]) {
+    if (values == null) {
+      this.propertiesDataSource.data = titles.map(title => {
+        return { title, value: { title, product: null, id: null, value: null } };
+      });
+    } else {
+      this.propertiesDataSource.data = titles.map(title => {
+        const result = values.find(x => x.title.id === title.id);
+        const value = result ? result : { title, product: null, id: null, value: null };
+        return { title, value };
+      });
+    }
+  }
+
+  getDictionaryValues(dictionaryId: number) {
+    return this.dictionaryService.getDictionaryValues(dictionaryId);
+  }
+
+  ngOnInit() {
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  async saveProduct() {
+    await this.loadableImage.uploadImage();
+    this.product.values = this.propertiesDataSource.data.map(x => x.value);
+    this.productService.saveProduct(this.product).subscribe(
+      success => {
+        this.snack.open('Успешно сохранено!');
+        this.dialogRef.close();
+      },
+      error => {
+        console.error(error);
+        alert('Ошибочка при сохранении продукта!');
+      }
+    );
+  }
+
+}
--- techno-space-front/src/app/pages/page-admin/subpages/product-subpage/product-subpage.component.ts
+++ techno-space-front/src/app/pages/page-admin/subpages/product-subpage/product-subpage.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Product } from 'src/models/products/product';
+import { MatSnackBar, MatTableDataSource, MatPaginator, MatDialog } from '@angular/material';
+import { EditProductDialog } from './edit-product/edit-product-dialog';
 
 @Component({
   selector: 'app-product-subpage',
@@ -7,9 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductSubpageComponent implements OnInit {
 
-  constructor() { }
+  productDataSource = new MatTableDataSource<Product>();
+  displayedColumns = ['name', 'actions'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private productService: ProductService,
+              private snack: MatSnackBar,
+              private matDialog: MatDialog) { }
 
   ngOnInit() {
+    this.paginator.pageSize = 10;
+    this.updateProductsList();
+    this.setPageUpdateEvent();
+  }
+
+  setPageUpdateEvent() {
+    this.paginator.page.subscribe(
+      success => {
+        this.updateProductsList();
+      }
+    );
+  }
+
+  updateProductsList() {
+    this.productService.getProducts(null, this.paginator.pageIndex, this.paginator.pageSize).subscribe(
+      products => {
+        console.log('products', products);
+        this.productDataSource.data = products.content;
+        this.paginator.length = products.totalElements;
+      }
+    );
+  }
+
+  addProduct() {
+    this.matDialog.open(EditProductDialog, { data: null }).afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          this.snack.open(`Успешно обновлено!`);
+          this.updateProductsList();
+        }
+      },
+      (error) => {
+        alert('Ошибочка');
+        console.error(error);
+      }
+    );
+    this.snack.open(`Типо открытие панельки..`);
+  }
+
+  editProduct(product: Product) {
+    this.matDialog.open(EditProductDialog, { data: product.id }).afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          this.snack.open(`Успешно обновлено!`);
+          this.updateProductsList();
+        }
+      },
+      (error) => {
+        alert('Ошибочка');
+        console.error(error);
+      }
+    );
+    this.snack.open(`Типо открытие панельки..`);
+  }
+
+  deleteProduct(product: Product) {
+    this.productService.removeProduct(product.id).subscribe(
+      () => {
+        this.snack.open(`Успешно удален продукт "${product.name}"!`);
+        this.updateProductsList();
+      },
+      error => {
+        this.snack.open('Не удалось удалить продукт!');
+        console.log(error);
+      }
+    );
   }
 
 }
