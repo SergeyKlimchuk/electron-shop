1cd98363c306dc57605665e39dd999633dae0dc9
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-04-13 18:14:15 +0600

    Add api token for ipapi service

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   domain: 'http://localhost:4200',
+  ipapiToken: '08d5ca34db4e97c590360edbbe36fe5b',
   googleMapApiToken: 'AIzaSyC9cSzN5u_aolSc4HTkDDn08FoLHdrUubE'
 };
 
