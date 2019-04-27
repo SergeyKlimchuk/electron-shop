754cdd98ca3bc56bd69124ea74826659e7b00cb6
Sergey Klimchuk <sergeyklimchuk@protonmail.com>
2019-04-27 16:25:16 +0600

    Update footer design

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { ProductTypeService } from './services/product-type/product-type.service';
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,16 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'techno-space';
+
+  categories$ = this.productTypeService.getProductTypes().pipe(map(x => x.content.slice(0, 12)));
+
   constructor(private matIconRegistry: MatIconRegistry,
-              private domSanitizer: DomSanitizer) {
+              private domSanitizer: DomSanitizer,
+              private productTypeService: ProductTypeService) {
+    this.linkImages();
+  }
+
+  linkImages() {
     this.matIconRegistry.addSvgIcon(
       `google`,
       this.domSanitizer.bypassSecurityTrustResourceUrl(`../assets/icons/google.svg`)
